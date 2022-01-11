02b4efe3d2e52f28d20d4be70f8c38fdbf12fbfb
zhangjinbao <sanfen@fuyuncn.com>
2022-01-11 17:55:24 +0800

    优化编译脚本

--- scripts/Statup.bat
+++ scripts/Statup.bat
@@ -1,30 +1,23 @@
 @echo build and run
 @cd ..
 
-@echo call buildApp
-@call:buildApp logger
-@call:buildApp center
-@call:buildApp config
-@call:buildApp gateway
-@call:buildApp login
-@call:buildRT room /AppType 15  /AppID 1000 /CenterAddr 127.0.0.1:10001 /ListenOnAddr 0.0.0.0:11000
-@call:buildRT table /AppType 14  /AppID 2000 /CenterAddr 127.0.0.1:10001 /ListenOnAddr 0.0.0.0:12000
+@echo call build
+@call:build logger -Type=1 -Id=1 -CenterAddr="127.0.0.1:10050"
+@call:build center -Type=2 -Id=50 -CenterAddr="127.0.0.1:10050"
+@call:build config -Type=3 -Id=60 -CenterAddr="127.0.0.1:10050"
+@call:build gateway -Type=4 -Id=100 -CenterAddr="127.0.0.1:10050"
+@call:build login -Type=5 -Id=70 -CenterAddr="127.0.0.1:10050"
+@call:build list -Type=6 -Id=80 -CenterAddr="127.0.0.1:10050"
+@call:build property -Type=7 -Id=90 -CenterAddr="127.0.0.1:10050"
+@call:build table -Type=8 -Id=1000 -CenterAddr="127.0.0.1:10050"
+@call:build room -Type=9 -Id=2000 -CenterAddr="127.0.0.1:10050"
+@call:build robot -Type=10 -Id=3000 -CenterAddr="127.0.0.1:10050"
 @goto:eof
 
-:buildApp
-@set appName=%~1
-@if not exist .\cmd\%appName%\configs mkdir .\cmd\%appName%\configs\%appName%
-@xcopy .\configs\%appName%\ .\cmd\%appName%\configs\%appName%\ /s /f /h /y
-@cd .\cmd\%appName%\
-@go build
-@start .\%appName%.exe
-@cd ../..
-@goto:eof
-
-:buildRT
+:build
 @set appName=%~1
 @cd .\cmd\%appName%\
 @go build
-@start .\%appName%.exe %~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9
+@start .\%appName%.exe %~2=%~3 %~4=%~5 %~6=%~7
 @cd ../..
 @goto:eof
