42fb3dbf51e47a8fd188a52a18facc29d10b84ad
goldbaby <mrallen5511@sina.com>
2022-07-14 20:18:22 +0800

    移除core api

--- api/proto/build.bat
+++ api/proto/build.bat
@@ -1,10 +1,6 @@
 echo "go proto"
-@.\protoc.exe --go_out=.. gateway.proto
-@.\protoc.exe --go_out=.. center.proto
 @.\protoc.exe --go_out=.. lobby.proto
 @.\protoc.exe --go_out=.. types.proto
-@.\protoc.exe --go_out=.. logger.proto
-@.\protoc.exe --go_out=.. config.proto
 @.\protoc.exe --go_out=.. room.proto
 @.\protoc.exe --go_out=.. table.proto
 @.\protoc.exe --go_out=.. gameddz.proto
