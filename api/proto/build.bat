f063f067274cdee2ecfb8c5c07ac13877f12651f
Goldbaby <mrallen5511@sina.com>
2024-12-16 15:42:51 +0800

    暂时使用proto版本2.6

--- api/proto/build.bat
+++ api/proto/build.bat
@@ -1,10 +1,15 @@
 echo "go proto"
+
+@.\protoc.exe --go_out=.. center.proto
+@.\protoc.exe --go_out=.. config.proto
+@.\protoc.exe --go_out=.. gameddz.proto
+@.\protoc.exe --go_out=.. gateway.proto
+@.\protoc.exe --go_out=.. list.proto
 @.\protoc.exe --go_out=.. lobby.proto
-@.\protoc.exe --go_out=.. types.proto
+@.\protoc.exe --go_out=.. logger.proto
+@.\protoc.exe --go_out=.. property.proto
 @.\protoc.exe --go_out=.. room.proto
 @.\protoc.exe --go_out=.. table.proto
-@.\protoc.exe --go_out=.. gameddz.proto
-@.\protoc.exe --go_out=.. list.proto
-@.\protoc.exe --go_out=.. property.proto
+
 
 pause
