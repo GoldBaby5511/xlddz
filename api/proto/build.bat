6e2f9b2c7f6b6fadb35d00d562d9c38902827c7e
Goldbaby <mrallen5511@sina.com>
2024-12-19 16:34:52 +0800

    升级protobuf3.20.3

--- api/proto/build.bat
+++ api/proto/build.bat
@@ -11,5 +11,19 @@ echo "go proto"
 @.\protoc.exe --go_out=.. room.proto
 @.\protoc.exe --go_out=.. table.proto
 
+@.\protoc.exe --cpp_out=.. center.proto
+@.\protoc.exe --cpp_out=.. config.proto
+@.\protoc.exe --cpp_out=.. gameddz.proto
+@.\protoc.exe --cpp_out=.. gateway.proto
+@.\protoc.exe --cpp_out=.. list.proto
+@.\protoc.exe --cpp_out=.. lobby.proto
+@.\protoc.exe --cpp_out=.. logger.proto
+@.\protoc.exe --cpp_out=.. property.proto
+@.\protoc.exe --cpp_out=.. room.proto
+@.\protoc.exe --cpp_out=.. table.proto
+
+@.\protoc.exe --csharp_out=.. center.proto
+@.\protoc.exe --csharp_out=.. list.proto
+@.\protoc.exe --csharp_out=.. gateway.proto
 
 pause
